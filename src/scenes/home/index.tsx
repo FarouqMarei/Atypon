f9a1ea401bdf14497581a7da65f455a770384d97
Farouq <farouq.marei@gmail.com>
2024-09-09 23:19:02 +0300

    fix image case sensitive

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,8 +1,8 @@
 import useMediaQuery from "@/hooks/useMediaQuery";
 import { SelectedPage } from "@/shared/types";
 import HomePageGraphic from "@/assets/images/image2.png";
-import Vector from "@/assets/images/Vector.png";
-import Burger from "@/assets/images/Burger.png";
+import Vector from "@/assets/images/vector.png";
+import Burger from "@/assets/images/burger.png";
 import Search from "@/assets/images/search.png";
 import { useEffect, useState } from "react";
 import ToggleSwitch from "@/shared/ToggleSwitch";
